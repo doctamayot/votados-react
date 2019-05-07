4fb722ef00f8041a2500fc32e86629ab8e9c86bf
doctamayot <doctamayot@hotmail.com>
2019-05-07 08:16:48 -0500

    Updates

---
+++ precache-manifest.39c498a17992fc2c1042ea5434c0f4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bff56c7f717743dd139",
+    "url": "/votados-react/static/js/runtime~main.5bff56c7.js"
+  },
+  {
+    "revision": "af3acfcaa8fddc8a379b",
+    "url": "/votados-react/static/js/main.af3acfca.chunk.js"
+  },
+  {
+    "revision": "3472df6a8813e986633e",
+    "url": "/votados-react/static/js/1.3472df6a.chunk.js"
+  },
+  {
+    "revision": "af3acfcaa8fddc8a379b",
+    "url": "/votados-react/static/css/main.ef590fa7.chunk.css"
+  },
+  {
+    "revision": "5a505feb8562216adda5315c57566bbc",
+    "url": "/votados-react/index.html"
+  }
+];
